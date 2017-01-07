82bacb03c06f1b042df1317b9fe06c49d58a5be5
Simon Pichugin <spichugi@redhat.com>
2017-01-07 14:25:37 +0100

    Add basic CLI structure to the project

---
+++ cmd/get-scale.go
@@ -0,0 +1,42 @@
+// Copyright © 2017 Semen Pichugin <simon.pichugin@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// get-scaleCmd represents the get-scale command
+var getScaleCmd = &cobra.Command{
+	Use:   "get-scale",
+	Short: "Provides a music scale with chords that sound good",
+	Long: `With this command you can watch over some music scale
+and the chords that sound good with it.
+
+You can run it with a command:
+gozart get-scale --scale=major --key=C
+
+If you won't give it a scale or a key, it will ask for it.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// TODO: Work your own magic here
+		fmt.Println("get-scale was called")
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(getScaleCmd)
+}
